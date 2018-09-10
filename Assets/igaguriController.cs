1b9d7b64ec126e56c82f2965e7ae9ce27f97d60b
ysht-h <>
2018-09-10 17:23:03 +0900

    first commit

---
+++ Assets/igaguriController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class igaguriController : MonoBehaviour
+{
+
+    bool ishit = false;
+    float time = 0.0f;
+
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+
+        ishit = true;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        // shoot(new Vector3(0.0f, 200.0f, 2000.0f));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(ishit)
+        {
+            this.time += Time.deltaTime;
+            if( this.time > 2.0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
---
+++ Assets/igaguriGen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class igaguriGen : MonoBehaviour
+{
+    public GameObject igaguriPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriPrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worlddir = ray.direction;
+
+            Vector3 wind = new Vector3(Random.Range(-50.0f, 50.0f), Random.Range(-50.0f, 50.0f), 0.0f);
+
+            igaguri.GetComponent<igaguriController>().shoot((worlddir.normalized * 2000.0f) + wind);
+        }
+	}
+}
